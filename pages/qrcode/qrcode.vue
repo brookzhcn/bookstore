b2b68a1ec77021ce87c8a7ba08d5de718dbb1977
yangxu <zhyu@smail.nju.edu.cn>
2019-08-29 18:02:05 +0800

    add Qr code

--- pages/qrcode/qrcode.vue
+++ pages/qrcode/qrcode.vue
@@ -6,13 +6,13 @@
 	  <view class="tips">{{text}}</view>
 	</view>
 	<view class="input-container item">
-	  <input bindinput="bindKeyInput" placeholder="输入转换内容"/>
-	  <button type="default"  bindtap="changeText">提交</button>
+	  <input v-model="inputValue" placeholder="输入转换内容"/>
+	  <button type="default" @click="changeText">提交</button>
 	</view>
 	<view class="item">
-	  <button type="default"  bindtap="download">下载</button>
-	  <button type="default"  bindtap="repaint">设置x,y</button>
-	  <button type="default"  bindtap="getBase64Data">base64</button>
+	  <button type="default" @click="download">下载</button>
+	  <button type="default" @click="repaint">设置x,y</button>
+	  <button type="default" @click="getBase64Data">base64</button>
 	</view>
 	<view class="round left"></view>
 	<view class="round right"></view>
@@ -29,150 +29,182 @@
 <script>
 import drawQrcode from '../../utils/weapp.qrcode.js'
 
-Page({
-  data: {
-    text: 'https://m.baidu.com',
-    inputValue: ''
-  },
-  onLoad () {
-    this.draw()
-  },
-  changeText (text) {
-    if (!this.data.inputValue) {
-      wx.showModal({
-        title: '提示',
-        content: '请先输入要转换的内容！',
-        showCancel: false
-      })
-      return
-    }
-    this.setData({
-      text: this.data.inputValue
-    })
-    this.draw()
-  },
-  bindKeyInput (e) {
-    this.setData({
-      inputValue: e.detail.value
-    })
-  },
-  draw () {
-    drawQrcode({
-      width: 160,
-      height: 160,
-      x: 20,
-      y: 20,
-      canvasId: 'myQrcode',
-      // ctx: wx.createCanvasContext('myQrcode'),
-      typeNumber: 10,
-      text: this.data.text,
-      image: {
-        imageResource: '../../images/icon.png',
-        dx: 70,
-        dy: 70,
-        dWidth: 60,
-        dHeight: 60
-      },
-      callback(e) {
-        console.log('e: ', e)
-      }
-    })
-  },
-  repaint () {
-    // 设置二维码起始位置 x,y
-    drawQrcode({
-      width: 160,
-      height: 160,
-      x: 20,
-      y: 20,
-      canvasId: 'myQrcode',
-      typeNumber: 10,
-      text: this.data.text,
-      callback(e) {
-        console.log('e: ', e)
-      }
-    })
-  },
-  download () {
-    // 导出图片
-    wx.canvasToTempFilePath({
-      x: 0,
-      y: 0,
-      width: 300,
-      height: 300,
-      destWidth: 300,
-      destHeight: 300,
-      canvasId: 'myQrcode',
-      success(res) {
-        console.log('图片的临时路径为：', res.tempFilePath)
-        let tempFilePath = res.tempFilePath
-        // 保存图片，获取地址
-        // wx.saveFile({
-        //   tempFilePath,
-        //   success (res) {
-        //     const savedFilePath = res.savedFilePath
-        //     console.log('savedFilePath', savedFilePath)
-        //   }
-        // })
 
-        // 保存到相册
-        wx.saveImageToPhotosAlbum({
-          filePath: tempFilePath,
-          success: function (res) {
-            wx.showToast({
-              title: '保存成功',
-              icon: 'success',
-              duration: 2000
-            })
-          },
-          fail: function (res) {
-            wx.showToast({
-              title: '保存失败',
-              icon: 'none',
-              duration: 2000
-            })
-          }
-        })
-      }
-    })
-  },
-  getBase64Data () {
-    // 获取二维码 base64 格式
-    wx.canvasToTempFilePath({
-      x: 0,
-      y: 0,
-      width: 300,
-      height: 300,
-      destWidth: 300,
-      destHeight: 300,
-      canvasId: 'myQrcode',
-      success(res) {
-        console.log('图片的临时路径为：', res.tempFilePath)
-        let tempFilePath = res.tempFilePath
-        // 获取 base64
-        wx.getFileSystemManager().readFile({
-          filePath: tempFilePath,
-          encoding: 'base64',
-          success: function (res) {
-            console.log('[base64 data is]', res)
-            wx.showToast({
-              title: '获取成功',
-              icon: 'success',
-              duration: 2000
-            })
-          },
-          fail: function (res) {
-            wx.showToast({
-              title: '获取失败',
-              icon: 'none',
-              duration: 2000
-            })
-          }
-        })
-      }
-    })
-  }
-})
+export default {
+	data() {
+		return {
+			text: 'https://m.baidu.com',
+			inputValue: ''
+		};
+	},
+	created(){
+		drawQrcode({
+		  width: 160,
+		  height: 160,
+		  x: 20,
+		  y: 20,
+		  canvasId: 'myQrcode',
+		  // ctx: wx.createCanvasContext('myQrcode'),
+		  typeNumber: 10,
+		  text: this.text,
+		  image: {
+		    imageResource: '../../static/icon.png',
+		    dx: 70,
+		    dy: 70,
+		    dWidth: 60,
+		    dHeight: 60
+		  },
+		  callback(e) {
+		    console.log('e: ', e)
+		  }
+		})
+	},
+	methods:{
+		// onLoad () {
+		//   this.draw()
+		// },
+		changeText (text) {
+		  if (!this.inputValue) {
+		    wx.showModal({
+		      title: '提示',
+		      content: '请先输入要转换的内容！',
+		      showCancel: false
+		    })
+		    return
+		  }
+		  // this.setData() ({
+		  //   text: this.inputValue
+		  // })
+		  console.log("1")
+		  this.draw()
+		  console.log(this.inputValue)
+		},
+		// bindKeyInput (e) {
+		//   this.setData({
+		//     inputValue: e.detail.value
+		//   })
+		// },
+	
+		draw () {
+		  drawQrcode({
+		    width: 160,
+		    height: 160,
+		    x: 20,
+		    y: 20,
+		    canvasId: 'myQrcode',
+		    // ctx: wx.createCanvasContext('myQrcode'),
+		    typeNumber: 10,
+		    text: this.inputValue,
+		    image: {
+		      imageResource: '../../static/icon.png',
+		      dx: 70,
+		      dy: 70,
+		      dWidth: 60,
+		      dHeight: 60
+		    },
+		    callback(e) {
+		      console.log('e: ', e)
+		    }
+		  })
+		},
+		repaint () {
+		  // 设置二维码起始位置 x,y
+		  drawQrcode({
+		    width: 160,
+		    height: 160,
+		    x: 20,
+		    y: 20,
+		    canvasId: 'myQrcode',
+		    typeNumber: 10,
+		    text: this.text,
+		    callback(e) {
+		      console.log('e: ', e)
+		    }
+		  })
+		},
+		download () {
+		  // 导出图片
+		  wx.canvasToTempFilePath({
+		    x: 0,
+		    y: 0,
+		    width: 300,
+		    height: 300,
+		    destWidth: 300,
+		    destHeight: 300,
+		    canvasId: 'myQrcode',
+		    success(res) {
+		      console.log('图片的临时路径为：', res.tempFilePath)
+		      let tempFilePath = res.tempFilePath
+		      // 保存图片，获取地址
+		      // wx.saveFile({
+		      //   tempFilePath,
+		      //   success (res) {
+		      //     const savedFilePath = res.savedFilePath
+		      //     console.log('savedFilePath', savedFilePath)
+		      //   }
+		      // })
+		
+		      // 保存到相册
+		      wx.saveImageToPhotosAlbum({
+		        filePath: tempFilePath,
+		        success: function (res) {
+		          wx.showToast({
+		            title: '保存成功',
+		            icon: 'success',
+		            duration: 2000
+		          })
+		        },
+		        fail: function (res) {
+		          wx.showToast({
+		            title: '保存失败',
+		            icon: 'none',
+		            duration: 2000
+		          })
+		        }
+		      })
+		    }
+		  })
+		},
+		getBase64Data () {
+		  // 获取二维码 base64 格式
+		  wx.canvasToTempFilePath({
+		    x: 0,
+		    y: 0,
+		    width: 300,
+		    height: 300,
+		    destWidth: 300,
+		    destHeight: 300,
+		    canvasId: 'myQrcode',
+		    success(res) {
+		      console.log('图片的临时路径为：', res.tempFilePath)
+		      let tempFilePath = res.tempFilePath
+		      // 获取 base64
+		      wx.getFileSystemManager().readFile({
+		        filePath: tempFilePath,
+		        encoding: 'base64',
+		        success: function (res) {
+		          console.log('[base64 data is]', res)
+		          wx.showToast({
+		            title: '获取成功',
+		            icon: 'success',
+		            duration: 2000
+		          })
+		        },
+		        fail: function (res) {
+		          wx.showToast({
+		            title: '获取失败',
+		            icon: 'none',
+		            duration: 2000
+		          })
+		        }
+		      })
+		    }
+		  })
+		}
+		
+	}
+}
+
 </script>
 
 <style>
